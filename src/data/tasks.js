907bd8447df597e61b7a302fc595649e04a98371
Guillermo Garcia <guillermogarvar@hotmail.com>
2022-10-19 09:50:52 -0400

    primer commit

---
+++ src/data/tasks.js
@@ -0,0 +1,27 @@
+export const tasks = [
+    {
+        id: 0,
+        title: "Primera Tarea",
+        description: "Primera Tarea",
+    },
+    {
+        id: 1,
+        title: "Segunda Tarea",
+        description: "Segunda Tarea",
+    },
+    {
+        id: 2,
+        title: "Tercera Tarea",
+        description: "Tercera Tarea",
+    },
+    {
+        id: 3,
+        title: "Cuarta Tarea",
+        description: "Cuarta Tarea",
+    },
+    {
+        id: 4,
+        title: "Quinta Tarea",
+        description: "Quinta Tarea",
+    }
+];
